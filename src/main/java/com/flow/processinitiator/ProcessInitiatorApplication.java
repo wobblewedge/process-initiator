a0c9e62953971787f1799177ddd793ef6c2b8e05
wobblewedge <andobrown71@gmail.com>
2019-06-25 15:53:26 -0400

    Spring Bootstrapped

---
+++ src/main/java/com/flow/processinitiator/ProcessInitiatorApplication.java
@@ -0,0 +1,13 @@
+package com.flow.processinitiator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessInitiatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessInitiatorApplication.class, args);
+	}
+
+}
