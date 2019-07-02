09568a7e3d68e4c1a10c1f44f7e2b330b43e7d5f
wobblewedge <andobrown71@gmail.com>
2019-07-02 09:24:37 -0400

    ProcessExporter nearly finished

--- src/main/java/com/flow/service/ProcessExportService.java
+++ src/main/java/com/flow/service/ProcessExportService.java
@@ -11,6 +11,8 @@ import org.xml.sax.SAXException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,35 +21,49 @@ import javax.xml.parsers.ParserConfigurationException;
 @RestController
 public class ProcessExportService {
 
-/*	@RequestMapping("/{id}")
-	public Rating getRatings(@PathVariable("id") String id ) {
-		return new Rating("ID", 3);
-	}
-*/
-	
+	/*
+	 * @RequestMapping("/{id}") public Rating getRatings(@PathVariable("id") String
+	 * id ) { return new Rating("ID", 3); }
+	 */
+
 	public static void main(String[] args) throws SAXException, IOException {
-	 Document doc = readProcess("src/main/resources/processes/vacation-request.bpmn20.xml");
-	 
-	 
-	System.out.println (doc.getDocumentElement().getNodeName() + " " + doc.getDocumentElement().getNodeValue());
+		Document doc = readProcess("src/main/resources/processes/vacation-request.bpmn20.xml");
+		NodeList nodeList = writeProcess(doc);
+	}
+
+	public static Document readProcess(String path) throws SAXException, IOException {
+		Document doc = null;
+		File xmlProcess = new File(path);
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			// use java xml parsers to digest file into xml format
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			doc = builder.parse(xmlProcess);
+			doc.getDocumentElement().normalize();
+			// now working with elements in the xml tree
+		} catch (ParserConfigurationException pce) {
+			System.out.println("Cannot parse file with this configuration on.");
+		}
+		return doc;
+	}
+	
+	private static NodeList writeProcess(Document document){
+		
+		NodeList nodeList = document.getDocumentElement().getChildNodes();
+		for(int i=0; i<nodeList.getLength(); i++) {
+			Node nNode = nodeList.item(i);
+			System.out.println("Node Name:" + nNode.getNodeName());
+			while(nNode.hasChildNodes()) {
+				NodeList subNodes = nNode.getChildNodes();
+				System.out.println(subNodes.item(i).getNodeValue().toString());
+			}
+		}
+		
+		return nodeList;
 		
 	}
-	static Document readProcess(String path) throws SAXException, IOException{
-	Document doc = null;
-	File xmlProcess = new File(path);
-	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	try {
-	//use java xml parsers to digest file into xml format
-	DocumentBuilder builder = factory.newDocumentBuilder();
-	doc = builder.parse(xmlProcess);
-	doc.getDocumentElement().normalize();
-	//now working with elements in the xml tree
-	} catch(ParserConfigurationException pce) {
-		System.out.println("Cannot parse file with this configuration on.");
-	}
-	return doc;
-	}
 	
+
 	@RequestMapping("/{name}")
 	public String processReader(@PathVariable("name") String name) {
 		return new String("Testing 1,2,3");
